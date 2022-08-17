37378532f3b7ccf779866a4d5789d51e1674297f
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2022-08-17 11:20:51 +0200

    Import Forrest Temple tileset and map

---
+++ src/main/java/com/bartlomiejpluta/demo/map/ForrestTempleHandler.java
@@ -0,0 +1,36 @@
+package com.bartlomiejpluta.demo.map;
+
+import com.bartlomiejpluta.base.api.context.Context;
+import com.bartlomiejpluta.base.api.input.Input;
+import com.bartlomiejpluta.base.api.map.model.GameMap;
+import com.bartlomiejpluta.base.api.map.handler.MapHandler;
+import com.bartlomiejpluta.base.api.screen.Screen;
+
+public class ForrestTempleHandler implements MapHandler {
+   public static final String UID = "f845355e-b9ad-4884-a217-dd3a4c18a3fa";
+
+   @Override
+   public void onCreate(Context context, GameMap map) {
+
+   }
+
+   @Override
+   public void onOpen(Context context, GameMap map) {
+
+   }
+
+   @Override
+   public void input(Input input) {
+
+   }
+
+   @Override
+   public void update(Context context, GameMap map, float dt) {
+
+   }
+
+   @Override
+   public void postRender(Screen screen) {
+
+   }
+}
